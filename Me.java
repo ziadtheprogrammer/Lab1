03ed2927fda32c069b7422505bdd89d7bf53c5bc
Ziad Kassab <zikozauiy@gmail.com>
2023-01-25 20:26:23 +0200

    Ziad Kassab - Mechatronics Engineer

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Face {
+    public static void main(String[] args)
+    {
+        System.out.println("Ziad Kassab:);
+	System.out.println("Mechatronics Engineer");
+    }
+}
---
+++ Problem.java
@@ -0,0 +1,6 @@
+public class Problem { 
+	public static void main (String[] args) {
+		System.out.print("Hello, I have a lot of problems here");
+		System.out.println("Can you help me in that please?");
+	}
+} 
